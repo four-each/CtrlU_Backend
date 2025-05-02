0bf295fccd8a1092462cb1aa9a5caf7997587e2c
yeonjookang <96986782+yeonjookang@users.noreply.github.com>
2025-05-02 18:25:11 +0900

    fix: 불필요한 주석 제거

--- src/main/java/org/example/ctrlu/healthy/HealthyController.java
+++ src/main/java/org/example/ctrlu/healthy/HealthyController.java
@@ -1,7 +1,6 @@
 package org.example.ctrlu.healthy;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
