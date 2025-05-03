76ce11e0bc38c7a813b154b77b0753fcb2223474
yeonjookang <96986782+yeonjookang@users.noreply.github.com>
2025-05-03 21:38:04 +0900

    fix: redis value 오류 수정

--- src/main/java/org/example/ctrlu/infra/redis/RedisConfig.java
+++ src/main/java/org/example/ctrlu/infra/redis/RedisConfig.java
@@ -19,10 +19,10 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @EnableRedisRepositories
 public class RedisConfig extends CachingConfigurerSupport {
 
-    @Value("${spring.redis.port}")
+    @Value("${spring.data.redis.port}")
     private int port;
 
-    @Value("${spring.redis.host}")
+    @Value("${spring.data.redis.host}")
     private String host;
 
     @Bean
