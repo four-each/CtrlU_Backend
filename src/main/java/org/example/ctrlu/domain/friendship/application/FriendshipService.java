ebd5d4cd2050e11adb7c61c87e92dbf6b15de881
정소민 <yjk70702002@gmail.com>
2025-06-12 11:19:16 +0900

    feat: 친구 신청 api 구현

--- src/main/java/org/example/ctrlu/domain/friendship/application/FriendshipService.java
+++ src/main/java/org/example/ctrlu/domain/friendship/application/FriendshipService.java
@@ -1,11 +1,38 @@
 package org.example.ctrlu.domain.friendship.application;
 
+import static org.example.ctrlu.domain.friendship.exception.FriendshipErrorCode.*;
+
 import org.example.ctrlu.domain.friendship.dto.request.FriendshipRequest;
+import org.example.ctrlu.domain.friendship.entity.Friendship;
+import org.example.ctrlu.domain.friendship.exception.FriendshipException;
+import org.example.ctrlu.domain.friendship.repository.FriendShipRepository;
+import org.example.ctrlu.domain.user.entity.User;
+import org.example.ctrlu.domain.user.entity.UserStatus;
+import org.example.ctrlu.domain.user.repository.UserRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import lombok.RequiredArgsConstructor;
 
 @Service
+@RequiredArgsConstructor
 public class FriendshipService {
-	public void requestFriendship(Long userId, FriendshipRequest request) {
+	private final FriendShipRepository friendShipRepository;
+	private final UserRepository userRepository;
 
+	@Transactional
+	public void requestFriendship(Long userId, FriendshipRequest request) {
+		User loginUser = userRepository.findByIdAndStatus(userId, UserStatus.ACTIVE)
+			.orElseThrow(() -> new FriendshipException(NOT_FOUND_USER));
+
+		User target = userRepository.findByIdAndStatus(request.targetId(), UserStatus.ACTIVE)
+			.orElseThrow(() -> new FriendshipException(NOT_FOUND_USER));
+
+		Friendship friendship = Friendship.builder()
+			.fromUser(loginUser)
+			.toUser(target)
+			.build();
+
+		friendShipRepository.save(friendship);
 	}
 }
--- src/main/java/org/example/ctrlu/domain/friendship/exception/FriendshipErrorCode.java
+++ src/main/java/org/example/ctrlu/domain/friendship/exception/FriendshipErrorCode.java
@@ -1,6 +1,7 @@
 package org.example.ctrlu.domain.friendship.exception;
 
 import org.example.ctrlu.global.response.ErrorCode;
+import org.springframework.http.HttpStatus;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -9,7 +10,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum FriendshipErrorCode implements ErrorCode {
 
-	; // FriendshipErrorCode
+	NOT_FOUND_USER(HttpStatus.BAD_REQUEST.value(),"F001","존재하지 않는 사용자입니다.");
 
 	private final int status;
 	private final String code;
