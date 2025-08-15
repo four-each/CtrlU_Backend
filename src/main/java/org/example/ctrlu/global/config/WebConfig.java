cd114497b4329eff7d73eb067f021d88a6a2d558
정소민 <yjk70702002@gmail.com>
2025-08-15 17:47:44 +0900

    feat: cors 설정

--- src/main/java/org/example/ctrlu/global/config/WebConfig.java
+++ src/main/java/org/example/ctrlu/global/config/WebConfig.java
@@ -1,19 +1,28 @@
 package org.example.ctrlu.global.config;
 
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-	@Override
-	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**") // 모든 경로에 대해
-			.allowedOrigins("https://ctrlu.site") // 허용할 출처
-			.allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS") // 허용할 메서드
-			.allowedHeaders("Authorization", "Content-Type", "Accept")
-			.exposedHeaders("Authorization", "Set-Cookie")
-			.allowCredentials(true)
-			.maxAge(3600);
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowedOrigins(List.of("https://ctrlu.site"));
+		config.setAllowedMethods(List.of("GET","POST","PUT","DELETE", "PATCH", "OPTIONS"));
+		config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+		config.setAllowCredentials(true);
+		config.setMaxAge(3600L);
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", config);
+		return source;
 	}
 }
