e3272c94aa60082f31ebfe5f3c17be52ae552f7f
정소민 <yjk70702002@gmail.com>
2025-08-15 18:10:33 +0900

    feat: cors 관련 설정 추가

--- src/main/java/org/example/ctrlu/global/config/WebConfig.java
+++ src/main/java/org/example/ctrlu/global/config/WebConfig.java
@@ -1,6 +1,12 @@
 package org.example.ctrlu.global.config;
 
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -15,5 +21,19 @@ public class WebConfig implements WebMvcConfigurer {
 			.exposedHeaders("Authorization", "Set-Cookie")
 			.allowCredentials(true)
 			.maxAge(3600);
+	}
+
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowedOrigins(List.of("https://ctrlu.site"));
+		config.setAllowedMethods(List.of("GET","POST","PUT","DELETE", "PATCH", "OPTIONS"));
+		config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+		config.setAllowCredentials(true);
+		config.setMaxAge(3600L); // 1시간 캐싱
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", config);
+		return source;
 	}
 }
