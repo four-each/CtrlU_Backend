397a1f1858c830a551586a7c880059855e90e797
정소민 <yjk70702002@gmail.com>
2025-08-15 18:01:25 +0900

    feat: cors 관련 설정 추가

--- src/main/java/org/example/ctrlu/global/config/SecutiryConfig.java
+++ src/main/java/org/example/ctrlu/global/config/SecutiryConfig.java
@@ -4,6 +4,7 @@ import org.example.ctrlu.global.security.JwtAuthenticationProvider;
 import org.example.ctrlu.global.security.JwtFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -41,6 +42,7 @@ public class SecutiryConfig {
 			.sessionManagement(session
 				-> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 			.authorizeHttpRequests(auth -> auth
+				.requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
 				.requestMatchers("/auth/signup",
 					"/auth/signin",
 					"/auth/reissue",
--- src/main/java/org/example/ctrlu/global/config/WebConfig.java
+++ src/main/java/org/example/ctrlu/global/config/WebConfig.java
@@ -1,28 +1,19 @@
 package org.example.ctrlu.global.config;
 
-import java.util.List;
-
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-	@Bean
-	public CorsConfigurationSource corsConfigurationSource() {
-		CorsConfiguration config = new CorsConfiguration();
-		config.setAllowedOrigins(List.of("https://ctrlu.site"));
-		config.setAllowedMethods(List.of("GET","POST","PUT","DELETE", "PATCH", "OPTIONS"));
-		config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
-		config.setAllowCredentials(true);
-		config.setMaxAge(3600L);
-
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		source.registerCorsConfiguration("/**", config);
-		return source;
+	@Override
+	public void addCorsMappings(final CorsRegistry registry) {
+		registry.addMapping("/**")
+			.allowedOrigins("https://ctrlu.site")
+			.allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+			.allowedHeaders("Authorization", "Content-Type", "Accept")
+			.exposedHeaders("Authorization", "Set-Cookie")
+			.allowCredentials(true)
+			.maxAge(3600);
 	}
 }
