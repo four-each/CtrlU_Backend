b4c619812790452186b519f177ac7e7ea9016535
정소민 <yjk70702002@gmail.com>
2025-08-25 18:18:07 +0900

    fix: 친구 검색 presigned url 반환

--- src/main/java/org/example/ctrlu/domain/user/dto/response/SearchUsersResponse.java
+++ src/main/java/org/example/ctrlu/domain/user/dto/response/SearchUsersResponse.java
@@ -1,6 +1,7 @@
 package org.example.ctrlu.domain.user.dto.response;
 
 import org.example.ctrlu.domain.user.entity.User;
+import org.example.ctrlu.global.s3.AwsS3Service;
 
 public record SearchUsersResponse(
 	Long id,
@@ -8,7 +9,12 @@ public record SearchUsersResponse(
 	String email,
 	String image
 ) {
-	public static SearchUsersResponse from(User user) {
-		return new SearchUsersResponse(user.getId(), user.getNickname(), user.getEmail(), user.getProfileImageKey());
+	public static SearchUsersResponse from(User user, AwsS3Service awsS3Service) {
+		return new SearchUsersResponse(
+			user.getId(),
+			user.getNickname(),
+			user.getEmail(),
+			awsS3Service.generateGetPresignedUrl(user.getProfileImageKey())
+		);
 	}
 }
--- src/main/java/org/example/ctrlu/domain/user/repository/UserRepository.java
+++ src/main/java/org/example/ctrlu/domain/user/repository/UserRepository.java
@@ -21,16 +21,15 @@ public interface UserRepository extends JpaRepository<User, Long> {
     String getImageById(@Param("id") Long id);
 
 	@Query("""
-    	SELECT u FROM User u
-    	WHERE LOWER(u.email) LIKE CONCAT(:keyword, '%@%')
-    	ORDER BY u.id ASC
-    """)
-	Slice<User> findByEmailStartsWithFirstPage(@Param("keyword") String keyword, Pageable pageable);
+		SELECT u FROM User u
+		WHERE (:cursorId IS NULL OR u.id > :cursorId)
+		  AND LOWER(u.email) LIKE CONCAT(:keyword, '%@%')
+		ORDER BY u.id ASC
+	""")
+	Slice<User> findByEmailWithCursor(
+		@Param("keyword") String keyword,
+		@Param("cursorId") Long cursorId,
+		Pageable pageable
+	);
 
-	@Query("""
-    	SELECT u FROM User u
-    	WHERE u.id > :cursorId AND LOWER(u.email) LIKE CONCAT(:keyword, '%@%')
-    	ORDER BY u.id ASC
-    """)
-	Slice<User> findByEmailStartsWithNextPage(@Param("keyword") String keyword, @Param("cursorId") Long cursorId, Pageable pageable);
 }
