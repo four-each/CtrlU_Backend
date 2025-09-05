d7b8342fd6eb8b2907e7eb76622dc53b2a31119a
정소민 <yjk70702002@gmail.com>
2025-09-05 19:00:13 +0900

    fix: 비밀번호 조건 변경

--- src/main/java/org/example/ctrlu/domain/auth/dto/request/SigninRequest.java
+++ src/main/java/org/example/ctrlu/domain/auth/dto/request/SigninRequest.java
@@ -9,8 +9,8 @@ public record SigninRequest (
 	@Email(message = "유효한 이메일 형식이어야 합니다.")
 	String email,
 	@Pattern(
-		regexp = "^(?=.*[a-zA-Z])(?=.*\\p{IsHangul})(?=.*[!@#$%^&*])[A-Za-z\\p{IsHangul}0-9!@#$%^&*]{8,12}$",
-		message = "비밀번호는 8~12자여야 하며, 영문, 한글, 특수문자를 각각 하나 이상 포함해야 합니다."
+		regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[!@#$%^&*])[A-Za-z\\d!@#$%^&*]{8,12}$",
+		message = "비밀번호는 8~12자여야 하며, 영문, 숫자, 특수문자를 각각 하나 이상 포함해야 합니다."
 	)
 	String password
 ) {
--- src/main/java/org/example/ctrlu/domain/user/dto/request/UpdatePasswordRequest.java
+++ src/main/java/org/example/ctrlu/domain/user/dto/request/UpdatePasswordRequest.java
@@ -4,13 +4,13 @@ import jakarta.validation.constraints.Pattern;
 
 public record UpdatePasswordRequest(
 	@Pattern(
-		regexp = "^(?=.*[a-zA-Z])(?=.*\\p{IsHangul})(?=.*[!@#$%^&*])[A-Za-z\\p{IsHangul}0-9!@#$%^&*]{8,12}$",
-		message = "비밀번호는 8~12자여야 하며, 영문, 한글, 특수문자를 각각 하나 이상 포함해야 합니다."
+		regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[!@#$%^&*])[A-Za-z\\d!@#$%^&*]{8,12}$",
+		message = "비밀번호는 8~12자여야 하며, 영문, 숫자, 특수문자를 각각 하나 이상 포함해야 합니다."
 	)
 	String currentPassword,
 	@Pattern(
-		regexp = "^(?=.*[a-zA-Z])(?=.*\\p{IsHangul})(?=.*[!@#$%^&*])[A-Za-z\\p{IsHangul}0-9!@#$%^&*]{8,12}$",
-		message = "비밀번호는 8~12자여야 하며, 영문, 한글, 특수문자를 각각 하나 이상 포함해야 합니다."
+		regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[!@#$%^&*])[A-Za-z\\d!@#$%^&*]{8,12}$",
+		message = "비밀번호는 8~12자여야 하며, 영문, 숫자, 특수문자를 각각 하나 이상 포함해야 합니다."
 	)
 	String newPassword
 ) {
